0d1f34750c1cfd3acd72eb2d282c9afc94472b6f
tjddnjs5898 <134911197+tjddnjs5898@users.noreply.github.com>
2025-07-28 16:33:21 +0900

    Update space.py

--- src/space.py
+++ src/space.py
@@ -1,17 +1,23 @@
 import cv2
 import numpy as np
 
+
 image = cv2.imread('../img/like_lenna.png')
-image = cv2.resize(image, (500,500))
-
-color = 255
-image = cv2.rectangle(image, (250, 150), (340, 230), color, 3,1 )
-image = cv2.rectangle(image, (370, 230), (450, 150), color, 3,1)
+image = cv2.resize(image, (500, 500))
 
 
-image = cv2.line(image, (340,190),(370,190), color, 3,1)
+red_color = (0, 0, 255)
 
-cv2.imshow('line',image)
 
+image = cv2.rectangle(image, (250, 150), (340, 230), red_color, 3, cv2.LINE_AA)
+
+
+image = cv2.rectangle(image, (370, 150), (450, 230), red_color, 3, cv2.LINE_AA)
+
+
+image = cv2.line(image, (340, 190), (370, 190), red_color, 3, cv2.LINE_AA)
+
+
+cv2.imshow('line', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
