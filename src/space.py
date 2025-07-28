f3b7e19c858cd11fab7b166854f4b776ccfd09ba
tjddnjs5898 <134911197+tjddnjs5898@users.noreply.github.com>
2025-07-28 16:31:07 +0900

    Update space.py

--- src/space.py
+++ src/space.py
@@ -5,9 +5,11 @@ image = cv2.imread('../img/like_lenna.png')
 image = cv2.resize(image, (500,500))
 
 color = 255
-image = cv2.rectangle(image, (250, 150), (340, 230), color, 3, 1)
-image = cv2.rectangle(image, (370, 230), (450, 150), color, 3, 1)
+image = cv2.rectangle(image, (250, 150), (340, 230), color, 3,1 )
+image = cv2.rectangle(image, (370, 230), (450, 150), color, 3,1)
 
+
+image = cv2.line(image, (340,190),(370,190), color, 3,1)
 
 cv2.imshow('line',image)
 
